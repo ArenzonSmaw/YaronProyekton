4bc5a15dc96c000ce2eaed856fc87b2c09ddb98f
Arenzon <arenzonlior@gmail.com>
2025-12-10 18:01:55 +0200

    Added Spring Bean project file

---
+++ YaronProjecton/src/main/java/deliveryTrio/YaronProjecton/YaronProjectonApplication.java
@@ -0,0 +1,13 @@
+package deliveryTrio.YaronProjecton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YaronProjectonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YaronProjectonApplication.class, args);
+	}
+
+}
---
+++ YaronProjecton/src/test/java/deliveryTrio/YaronProjecton/YaronProjectonApplicationTests.java
@@ -0,0 +1,13 @@
+package deliveryTrio.YaronProjecton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YaronProjectonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
